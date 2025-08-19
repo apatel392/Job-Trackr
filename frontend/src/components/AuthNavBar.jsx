661b3e22bd87868d68f269aed54b39b351f0a51a
akshar.26 <aksharpatel2601@gmail.com>
2025-08-19 00:44:23 -0400

    ec2 instance updated

--- frontend/src/components/AuthNavBar.jsx
+++ frontend/src/components/AuthNavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const AuthNavBar = () => {
     return (
-        <nav className='bg-blue-600 text-white px-6 py-3 shadow-md'>
+        <nav className='bg-gray-600 text-white px-6 py-3 shadow-md'>
             <div className='flex justify-between items-center'>
                 <Link to='/' className='text-xl font-bold'>
                     JobTrackr
